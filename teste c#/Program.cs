980586df5866fd66950bdf5ddcff324f4584d6d7
fabiomachado11 <99355891+fabiomachado11@users.noreply.github.com>
2023-04-01 10:06:32 -0300

    Add files via upload

---
+++ teste c#/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite o valor do produto: ");
+float valor = float.Parse(Console.ReadLine());
+
+float novoValor = valor * 60/100;
+float valorDesconto = valor - novoValor;
+
+Console.WriteLine("O produto custava: " + valor);
+Console.WriteLine("O novo valor com o desconto é: " + novoValor);
+Console.WriteLine("O valor do desconto foi: " + valorDesconto);
+
